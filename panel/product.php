c4f97f363e85ccab123fbdf977326fffe8ffd587
KillHosein <your-email@example.com>
2025-12-17 23:36:43 +0330

    4

--- panel/product.php
+++ panel/product.php
@@ -658,7 +658,4 @@ $todayDate = function_exists('jdate') ? jdate('l، j F Y') : date('Y-m-d');
                 $('#bulkDeleteForm').submit();
             });
         });
-    </script>
-
-</body>
-</html>
+    </scr
