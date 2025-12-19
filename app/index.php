77be925a0e1bb251a31813cf1f203daa113585da
KillHosein <your-email@example.com>
2025-12-19 23:11:26 +0330

    1

--- app/index.php
+++ app/index.php
@@ -3,12 +3,13 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>Mirza Web App</title>
     <script src="/app/js/telegram-web-app.js"></script>
     <script type="module" crossorigin src="/app/assets/index-C-2a0Dur.js"></script>
     <link rel="modulepreload" crossorigin href="/app/assets/vendor-CIGJ9g2q.js">
     <link rel="stylesheet" crossorigin href="/app/assets/index-BoHBsj0Z.css">
+    <link rel="stylesheet" href="/app/assets/custom.css">
   </head>
   <body>
     <div id="root"></div>
